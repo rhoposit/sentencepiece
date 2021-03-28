1a4ec93a267fc899e2e25adad83f6b214386aacc
Jennifer Williams <j.williams@ed.ac.uk>
2021-03-28 12:40:01 +0100

    update

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 folder=$1
 vsize=$2
 
-python convert_codes.py ${folder}/train.code ${folder}/train.code.conv
-python convert_codes.py ${folder}/dev.code ${folder}/dev.code.conv
-python convert_codes.py ${folder}/test.code ${folder}/test.code.conv
+#python convert_codes.py ${folder}/train.code ${folder}/train.code.conv
+#python convert_codes.py ${folder}/dev.code ${folder}/dev.code.conv
+#python convert_codes.py ${folder}/test.code ${folder}/test.code.conv
 
 #spm_train --input=${folder}/train.code.conv --model_prefix=models/${folder}.code.${vsize} --vocab_size=$vsize
 
@@ -11,9 +11,9 @@ python convert_codes.py ${folder}/test.code ${folder}/test.code.conv
 #spm_encode --model=models/${folder}.code.${vsize}.model --output_format=piece < ${folder}/dev.code.conv > ${folder}/dev.code.conv.${vsize}
 #spm_encode --model=models/${folder}.code.${vsize}.model --output_format=piece < ${folder}/test.code.conv > ${folder}/test.code.conv.${vsize}
 
-#python convert_clean.py ${folder}/train.code.conv.${vsize} ${folder}/train.code.conv.${vsize}.clean
-#python convert_clean.py ${folder}/dev.code.conv.${vsize} ${folder}/dev.code.conv.${vsize}.clean
-#python convert_clean.py ${folder}/test.code.conv.${vsize} ${folder}/test.code.conv.${vsize}.clean
+python convert_clean.py ${folder}/train.code.conv.${vsize} ${folder}/train.code.conv.${vsize}.clean
+python convert_clean.py ${folder}/dev.code.conv.${vsize} ${folder}/dev.code.conv.${vsize}.clean
+python convert_clean.py ${folder}/test.code.conv.${vsize} ${folder}/test.code.conv.${vsize}.clean
 
 
 
