6a5654f8e7369ca1e18457d64d39223f91993332
rhoposit <j.williams@ed.ac.uk>
2021-03-28 11:00:58 +0100

    update

--- plot_vocab.py
+++ plot_vocab.py
@@ -3,45 +3,31 @@
 import sys
 from collections import defaultdict
 
-infile = sys.argv[1]
-outfile = sys.argv[2]
+folder = sys.argv[1]
+outfile = ""
+Vsize = [256, 512, 1024, 2048, 4096]
+S = ["train", "valid", "test"]
+
+SetVocab = {"train":defaultdict(float), "valid":defaultdict(float), "test":defaultdict(float)}
+for s in S:
+    for v in Vsize:
+        UttLengths = []
+        Vocab = defaultdict(int)
+        infile = folder+"/"+s+".code.conv."+str(v)
+        input = open(infile, "r")
+        data = input.read().split("\n")[:-1]
+        input.close()
+        
+        for line in data:
+            substrings= line.split(" ")
+            UttLengths.append(len(substrings))
+            for s in substrings:
+                Vocab[s] += 1
+
+        total_vocab = len(list(Vocab.keys()))
+        avg_utt_length = sum(L) / float(len(L))
+        SetVocab[s][v] = avg_utt_length
 
 
-input = open(infile, "r")
-data = input.read().split("\n")[:-1]
-input.close()
-
-L = []
-large_L = 0
-output = open(outfile, "w")
-
-V = defaultdict(int)
-for line in data:
-    substrings= line.split(" ")
-    L.append(len(substrings))
-    if len(substrings) >= 1000:
-        large_L += 1
-    new_codes = []
-    for s in substrings:
-        V[s] += 1
-        substring = s.split("▁")
-        new_substring = []
-        for char in substring:
-            new_substring.append(char)
-        ns = "".join(new_substring)
-        new_codes.append(ns)
-    outstring = " ".join(new_codes)+"\n"
-#    print(outstring)
-    output.write(outstring)
-output.close()
-
-avg_len = sum(L) / float(len(L))
-min_len = min(L)
-max_len = max(L)
-
-print("CP avg len", avg_len)
-#print("CP min len", min_len)
-#print("CP max len", max_len)
-#print("CP num >= 1000", large_L)
-#print("CP total", len(L))
-print("CP unique vocab:", len(list(V.keys())))
+for fold,dist in SetVocab.items():
+    print(fold, dist)
